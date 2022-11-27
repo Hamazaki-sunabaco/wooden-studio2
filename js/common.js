b2982652a453890c6a90fe885c2c2a330c12c437
Hamazaki-sunabaco <114979196+Hamazaki-sunabaco@users.noreply.github.com>
2022-11-27 13:18:50 +0900

    2nd challenge

---
+++ js/common.js
@@ -0,0 +1,26 @@
+$(function(){
+
+    // AOS.js
+    AOS.init();
+    
+    //Vegas.js 
+    $(".main-visual").vegas({
+      timer: false,
+      transition: 'blur',
+      transitionDuration: 2000,
+      delay: 10000,
+      animation: 'kenburns',
+      animationDuration: 10000,
+      slides: [
+          { src: "./img/photo01.jpg" },
+          { src: "./img/photo011.jpg" },
+          { src: "./img/photo012.jpg" },
+      ]
+    });
+
+    // Drower.js
+    $(document).ready(function() {
+      $('.drawer').drawer();
+    });
+    
+});
